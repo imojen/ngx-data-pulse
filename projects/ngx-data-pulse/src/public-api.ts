bd22f7ac55e5fde6393546775273b1e0baab7e9c
Imojen <8040322+imojen@users.noreply.github.com>
2025-03-13 21:26:34 +0100

    refactor: Remove deprecated services and update package structure for ngx-data-pulse-workspace

---
+++ projects/ngx-data-pulse/src/public-api.ts
@@ -0,0 +1,23 @@
+/** Services */
+export * from "./lib/api";
+export * from "./lib/storage";
+export * from "./lib/notification";
+export * from "./lib/events";
+export * from "./lib/number";
+export * from "./lib/network";
+export * from "./lib/dates";
+export * from "./lib/idle";
+export * from "./lib/seo";
+export * from "./lib/modal";
+export * from "./lib/navigation";
+export * from "./lib/logs";
+export * from "./lib/platform";
+export * from "./lib/loader";
+export * from "./lib/scroller";
+
+/** Pipes NgX-Data-Pulse */
+export * from "./lib/_pipes";
+
+/** Module & Provider */
+export { NgxDataPulseModule } from "./lib/ngx-data-pulse.module";
+export { provideNgxDataPulse } from "./lib/ngx-data-pulse.provider";
